bddd5a880477037fef88009f9cf3fb7e5352542b
neilabbiebebe <neilabbie123@gmail.com>
2026-02-20 11:35:15 +0800

    6th

--- api/[...all].js
+++ api/[...all].js
@@ -1,1 +1,9 @@
-﻿module.exports = require("../server");
+const app = require("../server");
+
+module.exports = (req, res) => {
+  if (typeof req.url === "string") {
+    if (req.url === "/api") req.url = "/";
+    else if (req.url.startsWith("/api/")) req.url = req.url.slice(4) || "/";
+  }
+  return app(req, res);
+};
